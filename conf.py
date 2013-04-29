5bc08bd8ce4b617ba1f1c2359f8181744baf782c
Christopher LILJENSTOLPE <git@cdl.asgaard.org>
2013-04-28 19:56:28 -0700

    changes to conf.py for shadowplay theme and sidebar

--- conf.py
+++ conf.py
@@ -2,13 +2,15 @@
 # This is your config file.  Please write in a valid python syntax!
 # See http://posativ.org//conf.py.html
 
-TITLE = ('shadow', 'play')
+TITLE = ('Missing the', 'forest')
 
-SITENAME = 'A blog subtitle'
-WWW_ROOT = 'http://example.com/'
+SITENAME = 'Musings on random topics'
+WWW_ROOT = 'http://www.asgaard.org/'
 
-AUTHOR = 'Anonymous'
-EMAIL = 'mail@example.com'
+AUTHOR = 'Christopher Liljenstolpe'
+EMAIL = 'blog@cdl.asgaard.org'
+
+DEPLOY_DIR = 's3://www.asgaard.org/'
 
 FILTERS = ['markdown+codehilite(css_class=highlight)', 'hyphenate', 'h1']
 VIEWS = {
@@ -32,8 +34,23 @@ VIEWS = {
 THEME = 'shadowplay'
 ENGINE = 'acrylamid.templates.jinja2.Environment'
 
+DISQUS_SHORTNAME = 'asgaard'
+
+DEFAULT_ORPHANS = 3
+
 # Tuples are (name, link)
 BLOGROLL = [
-    ('Acrylamid', 'http://posativ.org/acrylamid/'),
-    ('Yet another blogroll', 'http://example.com/')
+    ('Some of my photographs', 'http://gallery.liljenstolpe.org/'),
+    ('My Github repo', 'http://www.github.com/liljenstolpe')
 ]
+
+TAG_CLOUD_STEPS = 4
+TAG_CLOUD_MAX_ITEMS = 100
+TAG_CLOUD_START_INDEX = 0
+TAG_CLOUD_SHUFFLE = True
+
+DEPLOYMENT = {
+    "ls": "ls $OUTPUT_DIR",
+    "echo": "echo '$OUTPUT_DIR'",
+    "default": "s3cmd sync $OUTPUT_DIR/ $DEPLOY_DIR"
+}
